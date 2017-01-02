b2f581ec689eba9774ac4def18318307711c70eb
Supasate Choochaisri <supasate.c@gmail.com>
2017-01-03 02:08:13 +0700

    Add Card wrapepr

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text } from 'react-native'
+import Card from './Card'
 
 const AlbumDetail = ({ album }) => (
-  <View>
+  <Card>
     <Text>{album.title}</Text>
-  </View>
+  </Card>
 )
 
 export default AlbumDetail
