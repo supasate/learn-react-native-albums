0111948e278c0fe81ae2156515011bb4e7d442b3
Supasate Choochaisri <supasate.c@gmail.com>
2017-01-03 13:18:29 +0700

    Change title

--- index.ios.js
+++ index.ios.js
@@ -5,7 +5,7 @@ import AlbumList from './src/components/AlbumList'
 
 const App = () => (
   <View style={{ flex: 1 }}>
-    <Header headerText="Text" />
+    <Header headerText="Albums" />
     <AlbumList />
   </View>
 )
